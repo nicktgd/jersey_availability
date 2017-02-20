6ce4a6ec543fa68cc377221bb30bbcff753d5a18
Nick <changethis@atalaterdate.co.nz>
2017-02-20 12:59:42 -0700

    Updated shebang path, added TODO

--- suns_site_scraper.py
+++ suns_site_scraper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.4
+#!/usr/bin/env python3
 
 import requests
 import bs4
@@ -31,7 +31,7 @@ def get_site_content():
     try:
         res.raise_for_status()
     except:
-        # Log data instead of sending to stdout
+        # TODO: Log data instead of sending to stdout
         print('Something went wrong accessing the site')
     else:
         site_file = open(SITE_FILE, 'wb')
